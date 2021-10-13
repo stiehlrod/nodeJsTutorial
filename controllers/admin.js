86adb75d7f247d4923c6d9a58c44ef1260c02816
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2021-10-13 12:34:24 -0600

    Adds methods for the cart.

--- controllers/admin.js
+++ controllers/admin.js
@@ -58,3 +58,9 @@ exports.getEditProduct = (req, res, next) => {
         })
     })
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products');
+}
--- controllers/shop.js
+++ controllers/shop.js
@@ -33,18 +33,33 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
-    });
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            products.forEach(product => {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if (cartProductData) {
+                    cartProducts.push({productData: product, qty: cartProductData.qty });
+                }
+            })
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+        })
+    })
 }
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, (product) => {
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
         Cart.addProduct(prodId, product.price);
-    });
-    res.redirect('/cart');
+        res.redirect('/');
+    })
 };
 
 exports.getOrders= (req, res, next) => {
